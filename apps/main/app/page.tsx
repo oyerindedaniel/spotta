b8c329beb19852222969de477dc3d17b9a3ebc31
oyerindedaniel <oyerinde.daniel@yahoo.com>
2024-04-22 15:39:58 +0100

    chore: added shadcnui

--- apps/main/app/page.tsx
+++ apps/main/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@repo/ui";
+
 export default function Page(): JSX.Element {
   return (
     <main className="bg-red-300">
       <p>Daniel</p>
+      <Button type="button" size="lg">
+        Click
+      </Button>
     </main>
   );
 }
